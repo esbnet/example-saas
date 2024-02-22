e9b8a00b67601fb10bf3a0da72999c5bac1d7251
Edmilson Soares <esbnet@gmail.com>
2024-02-22 03:51:58 -0300

    fix: :adhesive_bandage: modify url production billing

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -4,7 +4,6 @@ import {
 } from "@/app/components/submmitions-button";
 import prisma from "@/app/lib/db";
 import { getStripeSession, stripe } from "@/app/lib/stripe";
-import { getData } from "@/app/services/getSubscribeData";
 import {
   Card,
   CardContent,
@@ -14,30 +13,39 @@ import {
 } from "@/components/ui/card";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { CheckCircle2 } from "lucide-react";
+import { unstable_noStore as noStore } from "next/cache";
 import { redirect } from "next/navigation";
 
 const featureItems = [
-  {
-    name: "Basic",
-    description: "Write as many notes",
-    price: 30,
-  },
-  {
-    name: "Pro",
-    description: "Write as many notes as you want",
-    price: 40,
-  },
-  {
-    name: "Enterprise",
-    description: "Write as many notes as you want and more",
-    price: 50,
-  },
+  { name: "Lorem Ipsum something" },
+  { name: "Lorem Ipsum something" },
+  { name: "Lorem Ipsum something" },
+  { name: "Lorem Ipsum something" },
+  { name: "Lorem Ipsum something" },
 ];
 
-export default async function Billing() {
+async function getData(userId: string) {
+  noStore();
+  const data = await prisma.subscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      status: true,
+      user: {
+        select: {
+          stripeCustomerId: true,
+        },
+      },
+    },
+  });
+
+  return data;
+}
+
+export default async function BillingPage() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-
   const data = await getData(user?.id as string);
 
   async function createSubscription() {
@@ -53,7 +61,7 @@ export default async function Billing() {
     });
 
     if (!dbUser?.stripeCustomerId) {
-      throw new Error("Unable ro get customer id");
+      throw new Error("Unable to get customer id");
     }
 
     const subscriptionUrl = await getStripeSession({
@@ -61,27 +69,24 @@ export default async function Billing() {
       domainUrl:
         process.env.NODE_ENV == "production"
           ? (process.env.PRODUCTION_URL as string)
-          : (process.env.KINDE_POST_LOGOUT_REDIRECT_URL as string),
+          : "http://localhost:3000",
       priceId: process.env.STRIPE_PRICE_ID as string,
     });
 
-    redirect(subscriptionUrl);
+    return redirect(subscriptionUrl);
   }
 
   async function createCustomerPortal() {
     "use server";
-
     const session = await stripe.billingPortal.sessions.create({
-      customer: data?.user?.stripeCustomerId as string,
+      customer: data?.user.stripeCustomerId as string,
       return_url:
         process.env.NODE_ENV === "production"
           ? (process.env.PRODUCTION_URL as string)
-          : (process.env.KINDE_POST_LOGIN_REDIRECT_URL as string),
+          : "http://localhost:3000/dashboard",
     });
 
-    const subscriptionUrl = session.url as string;
-
-    redirect(subscriptionUrl);
+    return redirect(session.url);
   }
 
   if (data?.status === "active") {
@@ -89,21 +94,23 @@ export default async function Billing() {
       <div className="grid items-start gap-8">
         <div className="flex items-center justify-between px-2">
           <div className="grid gap-1">
-            <h1 className="text-3xl md:text-4xl">Subscription</h1>
+            <h1 className="text-3xl md:text-4xl ">Subscription</h1>
             <p className="text-lg text-muted-foreground">
-              Settings reading your subscription
+              Settings reagding your subscription
             </p>
           </div>
         </div>
+
         <Card className="w-full lg:w-2/3">
           <CardHeader>
             <CardTitle>Edit Subscription</CardTitle>
             <CardDescription>
               Click on the button below, this will give you the opportunity to
-              change your payment and view your statement at the same time.
+              change your payment details and view your statement at the same
+              time.
             </CardDescription>
           </CardHeader>
-          <CardContent className="">
+          <CardContent>
             <form action={createCustomerPortal}>
               <StripePortal />
             </form>
@@ -124,26 +131,25 @@ export default async function Billing() {
           </div>
 
           <div className="mt-4 flex items-baseline text-6xl font-extrabold">
-            R$30 <span className="ml-1 text-2xl font-muted-foreground">/m</span>
+            $30 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
           </div>
-
-          <p className="mt-4 text-lg text-muted-foreground">
-            Write as many notes as you want for R$30 a Month
+          <p className="mt-5 text-lg text-muted-foreground">
+            Write as many notes as you want for $30 a Month
           </p>
         </CardContent>
-
-        <div className="flex-1 flex flex-col  justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10">
+        <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
           <ul className="space-y-4">
             {featureItems.map((item, index) => (
-              <li key={index} className="flex items-start">
-                <div>
+              <li key={index} className="flex items-center">
+                <div className="flex-shrink-0">
                   <CheckCircle2 className="h-6 w-6 text-green-500" />
                 </div>
-                <p className="ml-3 text-base">{item.description}</p>
+                <p className="ml-3 text-base">{item.name}</p>
               </li>
             ))}
           </ul>
-          <form action={createSubscription} className="w-full">
+
+          <form className="w-full" action={createSubscription}>
             <StripeSubscriptionCreationButton />
           </form>
         </div>
